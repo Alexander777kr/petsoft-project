58209220f05c74aabfcf6696c1c78fb9420cc04d
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-06-11 14:28:46 +0300

    cleanup: fix image

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -8,6 +8,7 @@ import PetFormBtn from './pet-form-btn';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { DEFAULT_PET_IMAGE } from '@/lib/constants';
 
 type PetFormProps = {
   actionType: 'add' | 'edit';
@@ -31,9 +32,7 @@ const petFormSchema = z
   })
   .transform((data) => ({
     ...data,
-    imageUrl:
-      data.imageUrl ||
-      'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png',
+    imageUrl: data.imageUrl || DEFAULT_PET_IMAGE,
   }));
 
 type TPetForm = z.infer<typeof petFormSchema>;
@@ -60,6 +59,7 @@ export default function PetForm({
         if (!result) return;
         onFormSubmission();
         const petData = getValues();
+        petData.imageUrl = petData.imageUrl || DEFAULT_PET_IMAGE;
 
         if (actionType === 'add') {
           await handleAddPet(petData);
