e6182229e6f618d9132d3df0c9034e35f9cb6b4e
Aleksandr Kabanskiy <alexander-777@inbox.ru>
2024-06-19 14:38:53 +0300

    fix webhook mistake

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,4 +1,8 @@
 export async function POST(request: Request) {
   const body = await request.text();
-  console.log(body);
+
+  //fulfill order
+  
+  // return 200 OK
+  return Response.json(null, {status: 200});
 }
